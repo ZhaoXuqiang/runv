695e10ee47f9e4a6a0b3a045d6782e6508dd74d5
Zhang Wei <zhangwei555@huawei.com>
2017-06-15 16:02:35 +0800

    Downgrade runtime-spec version
    Temporarily downgrade runtime spec version for adapting our internal
    docker version, as our docker version is still using 1.11.2 which is
    using old runtime spec.
    
    Signed-off-by: Zhang Wei <zhangwei555@huawei.com>

--- vendor/github.com/opencontainers/runtime-spec/specs-go/config.go
+++ vendor/github.com/opencontainers/runtime-spec/specs-go/config.go
@@ -45,7 +45,7 @@ type Process struct {
 	// relative to the container's root.
 	Cwd string `json:"cwd"`
 	// Capabilities are Linux capabilities that are kept for the process.
-	Capabilities *LinuxCapabilities `json:"capabilities,omitempty" platform:"linux"`
+	Capabilities []string `json:"capabilities,omitempty" platform:"linux"`
 	// Rlimits specifies rlimit options to apply to the process.
 	Rlimits []LinuxRlimit `json:"rlimits,omitempty" platform:"linux"`
 	// NoNewPrivileges controls whether additional privileges could be gained by processes in the container.
